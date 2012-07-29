4c184cf6ca8c7ecde1ecc4c3064beeaa94ccaee0
Michael Parenteau <prmparenteau@aol.com>
2012-07-29 13:08:20 -0400

    add behavior on avatar hover to show person name

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
 //= require_tree .
+
+$(document).ready(function() {
+  $('.avatar').hover(function() {
+    var name = $(this).attr('name')
+    $(this).append('<span class="avatar-tip">'+name+'</span>')
+    $('.avatar-tip').animate({opacity: 1, top: "-30px"}, 200)
+  }, function() {
+    $('.avatar-tip').animate({opacity: 0, top: "-40px"}, 200, function() {
+      $(this).remove('span')
+    })
+  });
+});
